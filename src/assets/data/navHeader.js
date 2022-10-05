be89fe596e9b9208682179f379832cf87f8e38cc
Denis-92 <d.mozenic@gmail.com>
2022-10-05 23:06:03 +0200

    fix 'new' from string to boolean

--- src/assets/data/navHeader.js
+++ src/assets/data/navHeader.js
@@ -2,36 +2,36 @@ export const navLinks = [
     {
         "link": "",
         "name": "Courses",
-        "new": "false",
+        "new": false,
     },
     {
         "link": "",
         "name": "Zoom",
-        "new": "true",
+        "new": true,
     },
     {
         "link": "",
         "name": "Pages",
-        "new": "false",
+        "new": false,
     },
     {
         "link": "",
         "name": "Bundles",
-        "new": "false",
+        "new": false,
     },
     {
         "link": "",
         "name": "Course Formats",
-        "new": "false",
+        "new": false,
     },
     {
         "link": "",
         "name": "Add Course",
-        "new": "true",
+        "new": true,
     },
     {
         "link": "",
         "name": "Demos",
-        "new": "false",
+        "new": false,
     },
 ]
