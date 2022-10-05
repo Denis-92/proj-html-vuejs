c693ff0e20ed525f2436d650d04f01119e29ba7c
Denis-92 <d.mozenic@gmail.com>
2022-10-05 22:25:32 +0200

    add nav menu content

---
+++ src/assets/data/navHeader.js
@@ -0,0 +1,37 @@
+export const navLinks = [
+    {
+        "link": "",
+        "name": "Courses",
+        "new": "false",
+    },
+    {
+        "link": "",
+        "name": "Zoom",
+        "new": "true",
+    },
+    {
+        "link": "",
+        "name": "Pages",
+        "new": "false",
+    },
+    {
+        "link": "",
+        "name": "Bundles",
+        "new": "false",
+    },
+    {
+        "link": "",
+        "name": "Course Formats",
+        "new": "false",
+    },
+    {
+        "link": "",
+        "name": "Add Course",
+        "new": "true",
+    },
+    {
+        "link": "",
+        "name": "Demos",
+        "new": "false",
+    },
+]
--- src/components/header/navigationMenu.vue
+++ src/components/header/navigationMenu.vue
@@ -1,14 +1,21 @@
 <template>
     <nav class="flex flex-between-x">
         <h2>NAVIGATION</h2>
-        <div>CIAO</div>
+        <ul class="flex">
+            <li v-for="elements in navLinks" :key="elements.name">{{ elements.name }}</li>
+        </ul>
     </nav>
 </template>
   
 
 <script>
+import { navLinks } from '@/assets/data/navHeader.js';
+
 export default {
     name: 'navigationMenu',
+    data() {
+        return { navLinks };
+    },
 }
 </script>
   
@@ -19,5 +26,9 @@ export default {
 nav {
     background-color: aqua;
     height: 100px;
+}
+
+li {
+    margin: 1rem;
 }
 </style>  
