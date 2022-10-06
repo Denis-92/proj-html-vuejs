667068cc3f0901b0d338f60783d960b394dd8293
Denis-92 <d.mozenic@gmail.com>
2022-10-06 15:50:53 +0200

    add typeFields content

--- src/components/main/typeFields.vue
+++ src/components/main/typeFields.vue
@@ -1,6 +1,13 @@
 <template>
-    <div>
-        <h2>TYPE FIELDS</h2>
+    <div class="little-container">
+        <div class="flex flex-between-x flex-wrap" id="image-container">
+            <img src="@/assets/img/images/cat_1-770x375.jpg" alt="Image of laptops" id="first-image">
+            <img src="@/assets/img/images/cat_2-370x155.jpg" alt="Image of painting colors">
+            <img src="@/assets/img/images/cat_3-370x155.jpg" alt="Image of laptop">
+            <img src="@/assets/img/images/cat_4-370x155.jpg" alt="Image of violin">
+            <img src="@/assets/img/images/cat_5-370x155.jpg" alt="Image of workout">
+            <img src="@/assets/img/images/cat_6-370x155.jpg" alt="Image of landscape">
+        </div>
     </div>
 </template>
   
@@ -13,8 +20,16 @@ export default {
   
 
 <style lang="scss" scoped>
-div {
-    background-color: aqua;
-    height: 200px;
+#image-container {
+    >* {
+        width: calc(100% / 3 - 1rem);
+        padding: 1rem 0;
+    }
+
+    #first-image {
+        width: calc(100% / 3 * 2);
+    }
+
+
 }
 </style>  
