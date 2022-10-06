e09cea15926bc14c15c22ae4f1533837e0aab5b8
Denis-92 <d.mozenic@gmail.com>
2022-10-06 17:22:33 +0200

    add flex items container

--- src/components/main/typeFields.vue
+++ src/components/main/typeFields.vue
@@ -1,12 +1,24 @@
 <template>
     <div class="little-container">
         <div class="flex flex-between-x flex-wrap" id="image-container">
-            <img src="@/assets/img/images/cat_1-770x375.jpg" alt="Image of laptops" id="first-image">
-            <img src="@/assets/img/images/cat_2-370x155.jpg" alt="Image of painting colors">
-            <img src="@/assets/img/images/cat_3-370x155.jpg" alt="Image of laptop">
-            <img src="@/assets/img/images/cat_4-370x155.jpg" alt="Image of violin">
-            <img src="@/assets/img/images/cat_5-370x155.jpg" alt="Image of workout">
-            <img src="@/assets/img/images/cat_6-370x155.jpg" alt="Image of landscape">
+            <div id="first-image" class="flex-no-stretch">
+                <img src="@/assets/img/images/cat_1-770x375.jpg" alt="Image of laptops">
+            </div>
+            <div class="flex-no-stretch">
+                <img src="@/assets/img/images/cat_2-370x155.jpg" alt="Image of painting colors">
+            </div>
+            <div class="flex-no-stretch">
+                <img src="@/assets/img/images/cat_3-370x155.jpg" alt="Image of laptop">
+            </div>
+            <div>
+                <img src="@/assets/img/images/cat_4-370x155.jpg" alt="Image of violin">
+            </div>
+            <div>
+                <img src="@/assets/img/images/cat_5-370x155.jpg" alt="Image of workout">
+            </div>
+            <div>
+                <img src="@/assets/img/images/cat_6-370x155.jpg" alt="Image of landscape">
+            </div>
         </div>
     </div>
 </template>
@@ -20,16 +32,22 @@ export default {
   
 
 <style lang="scss" scoped>
+img {
+    width: 100%;
+    height: auto;
+}
+
 #image-container {
     >* {
         width: calc(100% / 3 - 1rem);
-        padding: 1rem 0;
     }
 
     #first-image {
-        width: calc(100% / 3 * 2);
+        width: calc(100% / 3 * 2 - 1rem);
     }
+}
 
-
+.flex-no-stretch {
+    align-self: flex-start;
 }
 </style>  
