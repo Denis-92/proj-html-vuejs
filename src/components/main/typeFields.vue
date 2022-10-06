efa2d16e255a37f1b1c241c29c28c57ef2faf674
Denis-92 <d.mozenic@gmail.com>
2022-10-06 17:58:13 +0200

    fix: - big column images; - order display cat4, cat5.

--- src/components/main/typeFields.vue
+++ src/components/main/typeFields.vue
@@ -1,24 +1,34 @@
 <template>
     <div class="little-container">
         <div class="flex flex-between-x flex-wrap" id="image-container">
-            <div id="first-image" class="flex-no-stretch">
+
+            <div id="first-image">
                 <img src="@/assets/img/images/cat_1-770x375.jpg" alt="Image of laptops">
             </div>
-            <div class="flex-no-stretch">
-                <img src="@/assets/img/images/cat_2-370x155.jpg" alt="Image of painting colors">
-            </div>
-            <div class="flex-no-stretch">
-                <img src="@/assets/img/images/cat_3-370x155.jpg" alt="Image of laptop">
-            </div>
+
+            <!-- container for big column -->
             <div>
-                <img src="@/assets/img/images/cat_4-370x155.jpg" alt="Image of violin">
+                <div>
+                    <img src="@/assets/img/images/cat_2-370x155.jpg" alt="Image of painting colors">
+                </div>
+                <div>
+                    <img src="@/assets/img/images/cat_3-370x155.jpg" alt="Image of laptop">
+                </div>
             </div>
+            <!-- /container for big column -->
+
             <div>
-                <img src="@/assets/img/images/cat_5-370x155.jpg" alt="Image of workout">
+                <img src="@/assets/img/images/cat_5-370x155.jpg" alt="Image of violin">
             </div>
+
+            <div>
+                <img src="@/assets/img/images/cat_4-370x155.jpg" alt="Image of workout">
+            </div>
+
             <div>
                 <img src="@/assets/img/images/cat_6-370x155.jpg" alt="Image of landscape">
             </div>
+
         </div>
     </div>
 </template>
