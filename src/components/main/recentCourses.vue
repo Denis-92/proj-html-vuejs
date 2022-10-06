0c4a59f4e3018af10741a46022ccf3de2881e663
Denis-92 <d.mozenic@gmail.com>
2022-10-06 23:35:28 +0200

    add cards in recent courses

--- src/components/main/recentCourses.vue
+++ src/components/main/recentCourses.vue
@@ -1,7 +1,9 @@
 <template>
     <div class="big-container">
         <div class="flex flex-column flex-center-y">
+
             <h3>Recent courses</h3>
+
             <div class="flex flex-aroud-x">
                 <ul v-for="element in importedCategoriesCourses" :key="element.category">
                     <li>
@@ -11,6 +13,17 @@
                     </li>
                 </ul>
             </div>
+
+            <div class="flex flex-aroud-x flex-wrap flex-center-y">
+                <div v-for="element in importedCourses" :key="element.id" class="cards-container">
+                    <div>
+                        <img src="@/assets/img/images/photo-1461749280684-dccba630e2f6-544x322.jpeg"
+                            :alt="element.name">
+                        {{ element.name }}
+                    </div>
+                </div>
+            </div>
+
         </div>
     </div>
 </template>
@@ -28,8 +41,17 @@ export default {
   
 
 <style lang="scss" scoped>
-div {
-    background-color: blue;
-    height: 200px;
+.cards-container {
+    width: calc(100% / 6 - 1rem);
+    margin: 0.1rem;
+
+    >* {
+        margin: 0.2rem;
+    }
+}
+
+img {
+    width: 100%;
+    height: auto;
 }
 </style>  
