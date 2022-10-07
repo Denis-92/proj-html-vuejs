6d2a4d45b282aaebcc096b51be73f7ea0730d34b
Denis-92 <d.mozenic@gmail.com>
2022-10-07 17:16:16 +0200

    add statistics content

--- src/components/main/statisticSuccess.vue
+++ src/components/main/statisticSuccess.vue
@@ -1,10 +1,32 @@
 <template>
     <div class="little-container">
-        <div class="flex flex-column flex-center-y">
+        <div class="flex flex-column flex-center-y" id="statistics">
+
             <h3>We are Proud</h3>
+
             <p>Certificate courses are instructed by highly educated and qualified instructors who hold
                 doctoral and master's level degrees.
             </p>
+
+            <div class="flex flex-around-x" id="numbers">
+                <div class="flex flex-column flex-center-y">
+                    <p>2000</p>
+                    <span>STUDENT</span>
+                </div>
+                <div class="flex flex-column flex-center-y">
+                    <p>950</p>
+                    <span>COURSES</span>
+                </div>
+                <div class="flex flex-column flex-center-y">
+                    <p>1600</p>
+                    <span>HOURS VIDEO</span>
+                </div>
+                <div class="flex flex-column flex-center-y">
+                    <p>150</p>
+                    <span>COUNTRIES REACHED</span>
+                </div>
+            </div>
+
         </div>
     </div>
 </template>
@@ -18,7 +40,22 @@ export default {
   
 
 <style lang="scss" scoped>
-div {
-    height: 200px;
+#statistics {
+    >* {
+        margin-top: 2rem;
+    }
+}
+
+#numbers {
+    padding-bottom: 15rem;
+
+    >div {
+        padding: 1rem;
+        border-left: 1px solid grey;
+    }
+
+    p {
+        font-size: 3rem;
+    }
 }
 </style>  
