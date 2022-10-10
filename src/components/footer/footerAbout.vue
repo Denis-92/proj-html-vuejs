d0fab8907154775d71db39b903bfe30c2cca12f3
Denis-92 <d.mozenic@gmail.com>
2022-10-10 13:10:25 +0200

    add footer about

---
+++ src/components/footer/footerAbout.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h3>About</h3>
+        <p>
+            <a href="#">Masterstudy</a> is Education WordPress theme
+            featured by Learning Management System
+            (LMS) for online education.
+            Developed by <a href="#">StylemixThemes</a>.
+        </p>
+    </div>
+</template>
+  
+
+<script>
+export default {
+    name: 'footerAbout',
+}
+</script>
+  
+
+<style lang="scss" scoped>
+
+</style>  
