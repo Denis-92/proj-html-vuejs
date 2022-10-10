97ce1d08b8496daf76da917767d039f461ef1716
Denis-92 <d.mozenic@gmail.com>
2022-10-10 14:58:46 +0200

    add images position absolute join CTA

--- src/components/main/joinCTA.vue
+++ src/components/main/joinCTA.vue
@@ -11,8 +11,12 @@
             </button>
         </div>
 
-        <div class="flex flex-center-y">
-            <img src="@/assets/img/images/base.png" alt="Image of a screen" class="relative">
+        <div class="flex flex-center-y relative">
+            <img src="@/assets/img/images/base.png" alt="Image of a screen">
+            <img src="@/assets/img/images/book1.png" alt="book with people" class="absolute" id="book1">
+            <img src="@/assets/img/images/book2.png" alt="book with children" class="absolute" id="book2">
+            <img src="@/assets/img/images/bubblespeech.png" alt="comics cloud" class="absolute" id="bubble-speech">
+            <img src="@/assets/img/images/magnifier.png" alt="Zoom lent" class="absolute" id="zoom-lent">
         </div>
     </div>
 </template>
@@ -39,5 +43,29 @@ export default {
 #start-investing {
     font-size: 3rem;
     font-weight: 300;
+}
+
+#book1 {
+    width: 10rem;
+    right: 1.5rem;
+    top: 4.5rem;
+}
+
+#book2 {
+    width: 10rem;
+    bottom: 2rem;
+    left: 11rem;
+}
+
+#bubble-speech {
+    width: 4rem;
+    right: 1rem;
+    top: 2rem;
+}
+
+#zoom-lent {
+    width: 3.5rem;
+    bottom: 5.5rem;
+    right: 3.5rem;
 }
 </style>  
