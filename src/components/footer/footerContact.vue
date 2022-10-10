77b8bffa1ef0a5d5c88f33f2118741fa3dba33af
Denis-92 <d.mozenic@gmail.com>
2022-10-10 13:10:55 +0200

    add footer contact

---
+++ src/components/footer/footerContact.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h3>Contact</h3>
+        <p>
+            USA, California 20, First Avenue,
+            California
+        </p>
+        <p>Tel.: +1 212 458 300 32</p>
+        <p>Fax: +1 212 375 24 14</p>
+        <a href="#">info@masterstudy.com</a>
+    </div>
+</template>
+  
+
+<script>
+export default {
+    name: 'footerContact',
+}
+</script>
+  
+
+<style lang="scss" scoped>
+
+</style>  
