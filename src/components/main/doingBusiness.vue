c1d3780d9696d122fa3eac6b8c8fb2992a0a0536
Denis-92 <d.mozenic@gmail.com>
2022-10-10 17:55:53 +0200

    add style to doing business component

--- src/components/main/doingBusiness.vue
+++ src/components/main/doingBusiness.vue
@@ -1,17 +1,20 @@
 <template>
-    <div>
+    <div class="flex flex-center-y flex-center-x" id="container-section">
 
-        <div class="flex">
+        <div>
+            <img src="@/assets/img/images/image_box_2-234x231.png" alt="colored computer screens">
+        </div>
 
-            <div class="flex flex-column">
-                <h2>Setup For Business</h2>
-                <p>
-                    Get unlimited access to 2,500 of Udemy's top
-                    courses for your team.
-                </p>
-                <button>DOING BUSINESS</button>
-            </div>
-
+        <div class="flex flex-column">
+            <h2>Setup For Business</h2>
+            <p>
+                Get unlimited access to 2,500 of Udemy's top courses for your team.
+            </p>
+            <button class="blue-button pill-button" id="button-doing-business">
+                <a href="#" class="white-font">
+                    DOING BUSINESS
+                </a>
+            </button>
         </div>
 
     </div>
@@ -30,13 +33,28 @@ button {
     width: 10rem;
 }
 
-// #jumbotron-text {
-//     width: 50%;
-//     font-size: 1.4rem;
-//     padding: 5rem 0;
+#button-doing-business {
+    padding: 0.8rem 0;
+}
 
-//     >div>* {
-//         margin: 1rem 0;
-//     }
-// }
+#container-section {
+    padding: 2rem 0.5rem;
+
+    >* {
+        margin: 0.5rem;
+
+        >* {
+            margin: 0.5rem;
+        }
+
+        h2 {
+            font-weight: 400;
+            font-size: 2rem;
+        }
+
+        p {
+            color: #888;
+        }
+    }
+}
 </style>  
